b3dca2a920c69cf4501b055e1e2c112ede28a685
nick-less <nick-less@gmx.de>
2020-03-31 16:03:31 +0200

    fixed tests

--- tests/js/test/test_shoppingcart_lib.js
+++ tests/js/test/test_shoppingcart_lib.js
@@ -1,22 +1,24 @@
 var assert = require('assert');
-var ShoppingCart = require('../../../src/js/shoppingcart_lib.js');
+
+import ShoppingCart from '../../../src/js/lib/shoppingcart.js'
+
 
 describe('ShoppingCart Lib', function() {
     describe('ShoppingCart.getCodeOfCountry()', function () {
         it('should return IT when the value is Italy', function () {
-            assert.equal('IT', window.ShoppingCart.getCodeOfCountry('Italy'));
+            assert.equal('IT', ShoppingCart.getCodeOfCountry('Italy'));
         });
         it('should return US when the value is United States', function () {
-            assert.equal('US', window.ShoppingCart.getCodeOfCountry('United States'));
+            assert.equal('US', ShoppingCart.getCodeOfCountry('United States'));
         });
     });
 
     describe('ShoppingCart.getContinentOfCountry()', function () {
         it('should return Europe when the value is Italy', function () {
-            assert.equal('Europe', window.ShoppingCart.getContinentOfCountry('Italy'));
+            assert.equal('Europe', ShoppingCart.getContinentOfCountry('Italy'));
         });
         it('should return North America when the value is United States', function () {
-            assert.equal('North America', window.ShoppingCart.getContinentOfCountry('United States'));
+            assert.equal('North America', ShoppingCart.getContinentOfCountry('United States'));
         });
     });
 });
